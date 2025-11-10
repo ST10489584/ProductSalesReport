2946b1ea5d50eddb8604ec2f566778d5f2737845
ST10489584 <ST10489584@vcconnect.edu.za>
2025-11-10 12:37:09 +0200

    Add ProductSalesInterface for sales operations

---
+++ ProductSalesInterface.java
@@ -0,0 +1,8 @@
+public interface ProductSalesInterface {
+    int[] GetProductSales();
+    int GetTotalSales();
+    int GetSalesOverLimit();
+    int GetSalesUnderLimit();
+    int GetProductsProcessed();
+    double GetAverageSales();
+}
