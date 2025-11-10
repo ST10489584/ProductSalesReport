8ca7436d92229d376a100af640e2deef0dd7ec38
ST10489584 <ST10489584@vcconnect.edu.za>
2025-11-10 12:52:44 +0200

    Enhance ProductSalesInterface with method comments
    Added comments to clarify method purposes in the interface.

--- ProductSalesInterface.java
+++ ProductSalesInterface.java
@@ -1,8 +1,9 @@
-public interface ProductSalesInterface {
-    int[] GetProductSales();
-    int GetTotalSales();
-    int GetSalesOverLimit();
-    int GetSalesUnderLimit();
-    int GetProductsProcessed();
-    double GetAverageSales();
+public interface ProductSalesInterface { //Defines methods for product GetSalesOverLimit
+    //Calulations
+    int[] GetProductSales(); //Returns array of product sales
+    int GetTotalSales(); //returns the total sales
+    int GetSalesOverLimit(); //returns the number of sales over sales limit 
+    int GetSalesUnderLimit(); //returns the number of sales under sales limit
+    int GetProductsProcessed(); //Returns number of years processed
+    double GetAverageSales(); //Returns the average sales rounded up
 }
